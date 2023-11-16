6c2425982e17b23c1fb28650dafc905c8de24c20
AbdulSami16k <abdulsami16000@gmail.com>
2023-11-16 16:08:37 +0500

    Navigation add

--- Spotify/App.tsx
+++ Spotify/App.tsx
@@ -1,13 +1,44 @@
+// App.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
-import AntDesign from "react-native-vector-icons/AntDesign"
-const App = () => {
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Explore from './component/Explore';
+import Home from './component/Home';
+import Profile from './component/Profile';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const Tab = createBottomTabNavigator();
+
+const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-      <AntDesign name="stepforward" size={40} color="red"/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarStyle: { backgroundColor: 'black' },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false, // Hides the header for all screens
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = '';
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home';
+            } else if (route.name === 'Explore') {
+              iconName = focused ? 'search1' : 'search1';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'user' : 'user';
+            }
+
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Explore" component={Explore} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 export default App;
