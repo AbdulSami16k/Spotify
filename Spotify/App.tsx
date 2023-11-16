56114c1669f1b57a290a0f92b71a2c9457683b8b
AbdulSami16k <abdulsami16000@gmail.com>
2023-11-16 15:01:55 +0500

    Icon add

--- Spotify/App.tsx
+++ Spotify/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import AntDesign from "react-native-vector-icons/AntDesign"
 const App = () => {
   return (
     <View>
       <Text>Hello World</Text>
+      <AntDesign name="stepforward" size={40} color="red"/>
     </View>
   );
 }
